f080121b0a9ef588be6bb110d3cae4731f011205
Deepakkulkarni18 <137081123+Deepakkulkarni18@users.noreply.github.com>
2025-10-06 09:43:19 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignment 0/Assignment 0/Ques3.c.c
@@ -0,0 +1,12 @@
+//Write a C program to convert temperature from Celsius to Fahrenheit using the
+formula:
+F = (C *9/5) + 32
+
+
+
+#include<stdio.h>
+void main (){
+float C = 35.45;                         
+float F = (9*C/5)+32;
+printf("Temperature in Fahrenheit is: %f\n",F);
+}
---
+++ C Programming/Assignments/Assignment 0/Assignment 0/Ques6.c.c
@@ -0,0 +1,12 @@
+//  Write a C program to find the square and cube of a given number.
+
+
+#include<stdio.h>
+void main (){
+ int s2 = 5;
+    int c3 = 3;
+    int square = s2*s2;
+    int cube = c3*c3*c3;
+    printf("The square of given number %d is: %d\n",s2,square);
+    printf("The cube of given number %d is: %d\n",c3,cube);
+}
